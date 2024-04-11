462450a6ab0899e73f8598e0512f1ece17d1690d
AT <ashutoshkumar63041@gmail.com>
2024-04-11 16:06:18 +0530

    tmp

--- obj/Debug/net6.0/KVIPSR_RAIPUR.AssemblyInfo.cs
+++ obj/Debug/net6.0/KVIPSR_RAIPUR.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KVIPSR_RAIPUR")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a11952c267f8e7450211dbfefe9199620f6b49b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc87c3a4913abfa7d6fa677308175883fab30e31")]
 [assembly: System.Reflection.AssemblyProductAttribute("KVIPSR_RAIPUR")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KVIPSR_RAIPUR")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
